d15fbc46adc340bb4da3e5c23dd6140b7957ddcc
Thiago Pontes <113463725+ThPontes@users.noreply.github.com>
2025-03-12 16:22:08 -0300

    capturar o nome e guardar na array

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeNomesSorteados = [];
+
+function adicionarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    listaDeNomesSorteados.push(amigo)
+    console.log(listaDeNomesSorteados)
+}
